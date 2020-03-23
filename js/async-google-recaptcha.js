b91bf3afbe212d30970a59c9c928c8a0b0a2d8a7
thex <andreas.kar@gmx.at>
2020-03-23 15:59:54 +0100

    improved code docs

--- js/async-google-recaptcha.js
+++ js/async-google-recaptcha.js
@@ -62,7 +62,7 @@
     * spinner.delay: Time in milliseconds waited before the spinner is removed.
     * isInViewport: Custom function to determine if container is in viewport (callback).
     * removeSpinner: Custom function to control and define the default spinner removal behavior (callback).
-    * triggerAsyncLoad: Custom function to define when the async load check is performed (callback).
+    * triggerAsyncLoad: Custom function to define when the reCAPTCHA should be loaded (callback).
     * checkAndLoad: Custom function which calls the async load and check routine (callback).
     * beforeLoad: Custom function called before the async load was initiated (callback).
     * afterLoad: Custom function called after the async load was initiated (callback).
